cbb37d9981a91fd87fc89b91f3d2a08dc0503f82
jodevdago <jogasy.rabefialy@gmail.com>
2025-06-04 12:24:54 +0300

    fix: update title

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -8,15 +8,18 @@ import { userGuard } from '@guards/user.guard';
 export const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
+    component: LoginComponent,
+    title: 'Support IT'
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    title: 'Support IT'
   },
   {
     path: 'unauthorized',
     component: UnauthorizedComponent,
+    title: 'Support IT | unauthorized'
   },
   {
     path: 'layout',
@@ -26,15 +29,18 @@ export const routes: Routes = [
       {
         path: '',
         loadComponent: () => import('./views/tickets/tickets.component').then(m => m.TicketsComponent),
+        title: 'Support IT | Tickets',
       },
       {
         path: 'tickets',
-        loadComponent: () => import('./views/tickets/tickets.component').then(m => m.TicketsComponent)
+        loadComponent: () => import('./views/tickets/tickets.component').then(m => m.TicketsComponent),
+        title: 'Support IT | Tickets',
       },
       {
         path: 'users',
         loadComponent: () => import('./views/users/users.component').then(m => m.UsersComponent),
-        canActivate: [userGuard]
+        canActivate: [userGuard],
+        title: 'Support IT | Users',
       },
     ]
   },
